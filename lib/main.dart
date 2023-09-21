0d0d1174049f76573a582acd08fe8b413ab7ac04
weviavek <we.viavek@gmail.com>
2023-09-21 05:50:38 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:student_management_website/initial/firebase_options.dart';
 
 import 'initial/my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyCfD3IMTbY0NNLBmbR5eJ97AjDciLlaeRY",
-          authDomain: "student-management-web-b973c.firebaseapp.com",
-          databaseURL:
-              "https://student-management-web-b973c-default-rtdb.firebaseio.com",
-          projectId: "student-management-web-b973c",
-          storageBucket: "student-management-web-b973c.appspot.com",
-          messagingSenderId: "235652828411",
-          appId: "1:235652828411:web:800ee2d28b7826bea4b45e"));
+      options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
